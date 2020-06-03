c5c41190868f030a24cae2c692ba61176632def6
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-06-03 17:23:16 -0400

    fix date on mainjs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 window.onload = () => {
-  'use strict';
+  "use strict";
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
   }
-}
+
+  function setUrl() {
+    var dateNow = new Date(Date.now());
+    var dateStr =
+      dateNow.getFullYear().valueOf() +
+      "-" +
+      (dateNow.getMonth().valueOf() + 1);
+
+    var url = "https://mirror.www.ahgora.com.br/#/dashboard/" + dateStr;
+
+    document.getElementById("iframe").setAttribute("src", url);
+  }
+
+  setUrl();
+};
